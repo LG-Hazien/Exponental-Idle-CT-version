c43817598491c797477955ce8de18bf711c2236f
LG-Hazien <144539093+LG-Hazien@users.noreply.github.com>
2023-09-09 14:12:41 -0300

    x

--- theory.js
+++ theory.js
@@ -12,8 +12,24 @@ var version = "1.0.0";
 
 var ft;
 
+const x = BigNumber.ZERO;
+
 var init = () => {
     ft = theory.createCurrency("f", "f");
+
+    ///////////////////
+    // Regular Upgrades
+
+    // x1
+    {
+        let getDesc = (level) => "x=" + x;
+        x1 = theory.createUpgrade(0, ft, new FreeCost());
+        x1.getDescription = (_) => Utils.getMath(getDesc(s1.level));
+        x1.getInfo = (amount) => Utils.getMathTo(getInfo(s1.level), getInfo(s1.level + amount));
+        x1.bought = (amount) => { 
+            x += 0.1  
+        };
+    }
 }
 
 init();
