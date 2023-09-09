c7bddd74e3bad9c839eaa6a59cc8724752d4eb1f
LG-Hazien <144539093+LG-Hazien@users.noreply.github.com>
2023-09-09 13:50:57 -0300

    Create theory.js

---
+++ theory.js
@@ -0,0 +1,13 @@
+import { ExponentialCost, FirstFreeCost, LinearCost } from "../api/Costs";
+import { Localization } from "../api/Localization";
+import { parseBigNumber, BigNumber } from "../api/BigNumber";
+import { theory } from "../api/Theory";
+import { Utils } from "../api/Utils";
+
+var id = "oWo";
+var name = "Exponential Idle";
+var description = "No Theory description.";
+var authors = "LG-Hazien";
+var version = "1.0.0";
+
+var ft;
